86622c840c30fff346a4a1bc58d8ca871f3913b6
Brett Toomey <brettcwx@gmail.com>
2017-01-10 16:00:46 -0800

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Sanitized",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
     ]
 )
